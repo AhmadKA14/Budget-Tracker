4bf281e54062871d3258ede9ec394a9c52078c3e
AhmadAbushawar <ahmad.abushawar@ontariotechu.net>
2022-10-22 20:24:53 -0400

    Configure React app for deployment to GitHub Pages

---
+++ src/components/UncategorizedBudgetCard.js
@@ -0,0 +1,13 @@
+import BudgetCard from "./BudgetCard";
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from "../contexts/BudgetsContext"
+
+export default function UncategorizedBudgetCard(props) {
+    const { getBudgetExpenses } = useBudgets()
+    const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce((total, expense) => total + expense.amount, 0)
+    console.log(amount);
+    if (amount === 0) { return null }
+
+    return (
+        <BudgetCard name="Uncategorized" amount={amount} gray {...props} />
+    )
+}
